925f7411da4b5ccd6437753abe456bc2d09170d1
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2020-04-19 21:04:54 -0400

    added in new signup categories and programmed some of their alerts

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -1,24 +1,41 @@
 document.getElementById("usernameSelectAlert").style.display = "none";
+document.getElementById("firstNameSelectAlert").style.display = "none";
+document.getElementById("lastNameSelectAlert").style.display = "none";
+document.getElementById("emailSelectAlert").style.display = "none";
 document.getElementById("passwordSelectAlert").style.display = "none";
+document.getElementById("confirmPasswordSelectAlert").style.display = "none";
 document.getElementById("colorSelectAlert").style.display = "none";
 document.getElementById("avatarnameSelectAlert").style.display = "none";
 document.getElementById("shapeSelectAlert").style.display = "none";
 
 document.getElementById("usernameInvalidAlert").style.display = "none";
+document.getElementById("firstNameInvalidAlert").style.display = "none";
+document.getElementById("lastNameInvalidAlert").style.display = "none";
+document.getElementById("emailInvalidAlert").style.display = "none";
 document.getElementById("passwordInvalidAlert").style.display = "none";
+document.getElementById("confirmPasswordInvalidAlert").style.display = "none";
 document.getElementById("avatarnameInvalidAlert").style.display = "none";
 
-document.getElementById("usernameTakenAlert").style.display = "none";;
+// taken alerts not coded yet
+document.getElementById("usernameTakenAlert").style.display = "none";
+document.getElementById("emailTakenAlert").style.display = "none";
 document.getElementById("avatarnameTakenAlert").style.display = "none";
 
 document.getElementById("fieldFillAlert").style.visibility = "hidden";
 
-//fix problem with submitting form without entering all required fields 
-//current testing solution: copy getting input like avatar color for all other fields and try to get to work
+/*need to code in input requirements checking for:
+  1) First Name(after username) - No spaces, first letter capital, alphabets only
+  2) Last Name(after first name) - No spaces, first letter capital, alphabets only
+  3) Email(after last name) - Get whether email is valid or not from HTML type="email" part
+*/
 
 var e = document.getElementById("colorSelector");
 var username = "";
-var password = ""
+var firstName = "";
+var lastName = "";
+var email = "";
+var password = "";
+var confirmPassword = "";
 var avatarColor = "";
 var avatarname = "";
 var avatarShape = "";
@@ -30,11 +47,26 @@ var shapes = ["circle", "triangle", "square", "pentagon", "hexagon"];
 function setUsername() {
   username = document.getElementById("usernameInput").value;
   console.log("Username: "+ username);
-
+}
+function setFirstName() {
+  firstName = document.getElementById("firstNameInput").value;
+  console.log("First Name: "+ firstName);
+}
+function setLastName() {
+  lastName = document.getElementById("lastNameInput").value;
+  console.log("Last Name: "+ lastName);
+}
+function setEmail() {
+  email = document.getElementById("emailInput").value;
+  console.log("Email: "+ email);
 }
 function setPassword() {
   password = document.getElementById("passwordInput").value;
   console.log("Password: "+ password);
+}
+function setConfirmPassword() {
+  confirmPassword = document.getElementById("confirmPasswordInput").value;
+  console.log("Confirm Password: "+ confirmPassword);
 }
 function setAvatarname() {
   avatarname = document.getElementById("avatarnameInput").value;
@@ -49,7 +81,11 @@ function setShape() {
 }
 function setValues() {
   setUsername();
+  setFirstName();
+  setLastName();
+  setEmail();
   setPassword();
+  setConfirmPassword();
   setColor();
   setAvatarname();
   setShape();
@@ -94,6 +130,8 @@ var passwordNum = false;
 var passwordUppercase = false;
 var passwordSpecial = false;
 
+var confirmCorrect = false;
+
 var usernameSpace = false;
 var passwordSpace = false;
 var avatarnameSpace = false;
@@ -120,6 +158,18 @@ function usernameRequirements() {
     document.getElementById("usernameInvalidAlert").style.display = "none";
     return true;
   }
+}
+
+function firstNameRequirements(){
+
+}
+
+function lastNameRequirements(){
+  
+}
+
+function emailRequirements(){
+
 }
 
 function pasSetup() {
@@ -167,6 +217,24 @@ function passwordRequirements() {
   }
 }
 
+function confirmPasswordRequirements() {
+  if((confirmPassword.localeCompare(password))!=0) {
+    confirmCorrect = false;
+  } else if((confirmPassword.localeCompare(password))==0) {
+    confirmCorrect = true;
+  }
+  if((confirmCorrect==false)) {
+    document.getElementById("confirmPasswordInvalidAlert").style.display = "inline";
+    return false;
+  } else if((confirmCorrect==true)){
+    document.getElementById("confirmPasswordInvalidAlert").style.display = "none";
+    return true;
+  } else {
+    document.getElementById("confirmPasswordInvalidAlert").style.display = "inline";
+    return false;
+  }
+}
+
 function avatarnameRequirements() {
   avatarnameLength = avatarname.length;
   avatarnamePars = avatarname.split("");
@@ -195,12 +263,48 @@ function usernameRequired() {
     return true;
   }
 }
+function firstNameRequired() {
+  if(firstName=="") {
+    document.getElementById("firstNameSelectAlert").style.display = "inline";
+    return false;
+  } else {
+    document.getElementById("firstNameSelectAlert").style.display = "none";
+    return true;
+  }
+}
+function lastNameRequired() {
+  if(lastName=="") {
+    document.getElementById("lastNameSelectAlert").style.display = "inline";
+    return false;
+  } else {
+    document.getElementById("lastNameSelectAlert").style.display = "none";
+    return true;
+  }
+}
+function emailRequired() {
+  if(email=="") {
+    document.getElementById("emailSelectAlert").style.display = "inline";
+    return false;
+  } else {
+    document.getElementById("emailSelectAlert").style.display = "none";
+    return true;
+  }
+}
 function passwordRequired() {
   if(password=="") {
     document.getElementById("passwordSelectAlert").style.display = "inline";
     return false;
   } else {
     document.getElementById("passwordSelectAlert").style.display = "none";
+    return true;
+  }
+}
+function confirmPasswordRequired() {
+  if(confirmPassword=="") {
+    document.getElementById("confirmPasswordSelectAlert").style.display = "inline";
+    return false;
+  } else {
+    document.getElementById("confirmPasswordSelectAlert").style.display = "none";
     return true;
   }
 }
@@ -232,14 +336,35 @@ function shapeRequired() {
     }
 }
 
+
 function usReqMet() {
   if(usernameRequired()==true) {
     return usernameRequirements();
   }
 }
+function fnmReqMet() {
+  if(firstNameRequired()==true) {
+    return firstNameRequirements();
+  }
+}
+function lnmReqMet() {
+  if(lastNameRequired()==true) {
+    return lastNameRequirements();
+  }
+}
+function emReqMet() {
+  if(emailRequired()==true) {
+    return emailRequirements();
+  }
+}
 function pwdReqMet() {
   if(passwordRequired()==true) {
     return passwordRequirements();
+  }
+}
+function confPwdReqMet() {
+  if(confirmPasswordRequired()==true) {
+    return confirmPasswordRequirements();
   }
 }
 function avReqMet() {
@@ -252,14 +377,17 @@ function avReqMet() {
 
 
 function checkSubmit() {
-  // || (pwdReqMet()==false)
-  if((usernameRequired()==false) || (passwordRequired()==false) || (avatarColorRequired()==false) || (avatarnameRequired()==false) || (shapeRequired()==false) ||
-  (usReqMet()==false) || (pwdReqMet()==false) || (avReqMet()==false)) {
+  // temporarily removed  || (usReqMet()==false) || (fnmReqMet()==false) || (lnmReqMet()==false) || (emReqMet()==false) || from  line 381
+  if((usernameRequired()==false) || (firstNameRequired()==false) || (lastNameRequired()==false) || (emailRequired()==false) || (passwordRequired()==false) || 
+  (confirmPasswordRequired()==false) || (avatarColorRequired()==false) || (avatarnameRequired()==false) || (shapeRequired()==false) (pwdReqMet()==false) || confPwdReqMet()==false || 
+  (avReqMet()==false)) {
     document.getElementById("fieldFillAlert").style.visibility = "visible";
     return false;
-  } else if((usernameRequired()==true) && (passwordRequired()==true) && (avatarColorRequired()==true) && (avatarnameRequired()==true) && (shapeRequired()==true) && 
-  (usReqMet()==true) && (pwdReqMet()==true) && (avReqMet()==true)) {
-    // && (pwdReqMet()==true)
+  // temporarily removed && (fnmReqMet()==true) && (lnmReqMet()==true) && (emReqMet()==true) from line 388
+  } else if((usernameRequired()==true) && (firstNameRequired()==true) && (lastNameRequired()==true) && (emailRequired()==true) && (passwordRequired()==true) && 
+  (confirmPasswordRequired()==true) && (avatarColorRequired()==true) && (avatarnameRequired()==true) && (shapeRequired()==true) && 
+  (usReqMet()==true) && (pwdReqMet()==true) && confPwdReqMet()==true && 
+  (avReqMet()==true)) {
     document.getElementById("fieldFillAlert").style.visibility = "hidden";
     return true;
   } else {
@@ -271,12 +399,20 @@ function checkSubmit() {
 function whenSubmit() {
   //setValues();
   usernameRequired();
+  firstNameRequired();
+  lastNameRequired();
+  emailRequired();
   passwordRequired();
+  confirmPasswordRequired();
   avatarColorRequired();
   avatarnameRequired();
   shapeRequired();
   usReqMet();
+  fnmReqMet();
+  lnmReqMet();
+  emReqMet();
   pwdReqMet();
+  confPwdReqMet();
   avReqMet();
   checkSubmit();
   if(checkSubmit()==false) {
